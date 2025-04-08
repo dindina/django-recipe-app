9142b7101fa6a474f926f3bece11a28df2e064dd
dinesh <dindina@gmail.com>
2025-04-08 15:51:55 +1000

    wait for db command code added

--- app/app/settings.py
+++ app/app/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'HOST': os.environ.get('DB_HOST'),
         'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DN_USER'),
+        'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASS')
     }
 }
--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -4,8 +4,24 @@ Django command to wait for DB to be available
 
 from django.core.management.base import BaseCommand
 
+import time
+
+from psycopg2 import OperationalError as PsyError
+
+from django.db.utils import OperationalError
+
 
 class Command(BaseCommand):
-    
+
     def handle(self, *args, **options):
-        pass
+        self.stdout.write('Waiting for DB Connection ->')
+        db_up = False
+        while not db_up:
+            try:
+                self.check(databases=['default'])
+                db_up = True
+            except (PsyError, OperationalError):
+                self.stdout.write('Wait for DB , waiting for a second')
+                time.sleep(1)
+
+        self.stdout.write('DB Available now! ')
