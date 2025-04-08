416d521c3c6cc2a91dd19f9da781b447cc4198ce
dinesh <dindina@gmail.com>
2025-04-08 12:46:55 +1000

    lint fixed

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,8 +1,9 @@
 from django.test import SimpleTestCase
 from app import calc
 
+
 class CalTests(SimpleTestCase):
 
     def test_add(self):
-        res = calc.add(5,6)
-        self.assertEquals(res,11)
+        res = calc.add(5, 6)
+        self.assertEquals(res, 11)
