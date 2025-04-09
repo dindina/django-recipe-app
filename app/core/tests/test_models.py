5b4c62607e43adc23075d8ce663f87df90bdd102
dinesh <dindina@gmail.com>
2025-04-09 15:07:43 +1000

    user api code

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,48 @@
+"""
+Test for models
+"""
+
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_successful(self):
+
+        email = "test@exampl.com"
+        password = "testpass123"
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password,
+        )
+
+        self.assertEqual(user.email,email)
+        self.assertTrue(user.check_password,password)
+
+
+    def test_new_user_email_normalised(self):
+
+        sample_emails = [
+            ['test1@EXAMPLE.com','test1@example.com']
+            ,['Test2@Example.com','Test2@example.com']
+            ,['TEST3@Example.com','TEST3@example.com']
+        ]
+
+        for email , expected in sample_emails:
+
+            user = get_user_model().objects.create_user(email,'samle123')
+            self.assertEqual(user.email,expected)
+
+
+    def test_new_user_without_email_raises_error(self):
+
+        with self.assertRaises(ValueError):
+            user = get_user_model().objects.create_user("",'samle123')
+
+
+    def test_new_create_super_user(self):
+
+        user = get_user_model().objects.create_superuser("test@example.com",'samle123')
+
+        self.assertEqual(user.is_superuser, True)
+        self.assertEqual(user.is_staff, True)
